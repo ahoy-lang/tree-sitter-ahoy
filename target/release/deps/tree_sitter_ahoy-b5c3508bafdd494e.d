a85e161c548998568502d31a6071895ffbd9bab3
kyle wannacott <WannacottL@gmail.com>
2025-11-26 19:40:44 +1300

    change outlines to oultine.scm

--- target/release/deps/tree_sitter_ahoy-b5c3508bafdd494e.d
+++ target/release/deps/tree_sitter_ahoy-b5c3508bafdd494e.d
@@ -1,8 +1,8 @@
-/home/lee/Documents/ahoy/tree-sitter-ahoy/target/release/deps/tree_sitter_ahoy-b5c3508bafdd494e.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json bindings/rust/../../queries/highlights.scm
+/home/lee/Documents/ahoy-lang/tree-sitter-ahoy/target/release/deps/tree_sitter_ahoy-b5c3508bafdd494e.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json bindings/rust/../../queries/highlights.scm
 
-/home/lee/Documents/ahoy/tree-sitter-ahoy/target/release/deps/libtree_sitter_ahoy-b5c3508bafdd494e.rlib: bindings/rust/lib.rs bindings/rust/../../src/node-types.json bindings/rust/../../queries/highlights.scm
+/home/lee/Documents/ahoy-lang/tree-sitter-ahoy/target/release/deps/libtree_sitter_ahoy-b5c3508bafdd494e.rlib: bindings/rust/lib.rs bindings/rust/../../src/node-types.json bindings/rust/../../queries/highlights.scm
 
-/home/lee/Documents/ahoy/tree-sitter-ahoy/target/release/deps/libtree_sitter_ahoy-b5c3508bafdd494e.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json bindings/rust/../../queries/highlights.scm
+/home/lee/Documents/ahoy-lang/tree-sitter-ahoy/target/release/deps/libtree_sitter_ahoy-b5c3508bafdd494e.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json bindings/rust/../../queries/highlights.scm
 
 bindings/rust/lib.rs:
 bindings/rust/../../src/node-types.json:
